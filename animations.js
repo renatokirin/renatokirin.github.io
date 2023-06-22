4db7b30e525af1bde40c9714918deeafa016cd0e
Renato Kirin <renatokirin@gmail.com>
2023-06-23 00:32:31 +0200

    Add responsive images

--- animations.js
+++ animations.js
@@ -1,3 +1,16 @@
+let lastScrollTop = 0;
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function () {
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-120px"; // hides the navbar
+  } else {
+    navbar.style.top = "0"; // shows the navbar
+  }
+  lastScrollTop = scrollTop;
+});
+
 function scrollToIntro() {
   const element = document.getElementById("intro-scroll");
   element.scrollIntoView({ behavior: "smooth" });
