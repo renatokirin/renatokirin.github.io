1baf9e251e5333945fe8fc52e58783c915cb19bd
Renato Kirin <renatokirin@gmail.com>
2023-06-23 00:08:11 +0200

    Remove animations

--- animations.js
+++ animations.js
@@ -1,32 +1,3 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-
-hiddenElements.forEach((el) => observer.observe(el));
-
-
-
-let lastScrollTop = 0;
-const navbar = document.getElementById('navbar');
-
-window.addEventListener('scroll', function () {
-  let scrollTop = window.scrollY || document.documentElement.scrollTop;
-  if (scrollTop > lastScrollTop) {
-    navbar.style.top = "-120px"; // hides the navbar
-  } else {
-    navbar.style.top = "0"; // shows the navbar
-  }
-  lastScrollTop = scrollTop;
-});
-
 function scrollToIntro() {
   const element = document.getElementById("intro-scroll");
   element.scrollIntoView({ behavior: "smooth" });
